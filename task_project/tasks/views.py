8f7c8c31667ef7529d36131c4d23e54580f784ca
sidharthnemchander <sidharthchander62@gmail.com>
2026-01-21 20:27:11 +0530

    learning about migrations

--- task_project/tasks/views.py
+++ task_project/tasks/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-
+from .models import Task
 
 def index(request):
-    return render(request,'tasks/index.html')
+    tasks = Task.objects.all()
+    return render(request, 'tasks/index.html', {'tasks': tasks})
