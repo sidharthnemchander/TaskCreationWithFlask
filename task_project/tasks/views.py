fc9390d44b3eaa58e711468d658ce4655610733e
sidharthnemchander <sidharthchander62@gmail.com>
2026-01-22 09:54:42 +0530

    redirection changes

--- task_project/tasks/views.py
+++ task_project/tasks/views.py
@@ -1,6 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Task
 
 def index(request):
+    if request.method == 'POST':
+        content = request.POST.get('content')
+        if content:
+            Task.objects.create(content=content)
+        return redirect('/')
+
     tasks = Task.objects.all()
     return render(request, 'tasks/index.html', {'tasks': tasks})
